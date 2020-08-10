dd92d2d9858bc2288052c31905c1cb24006de6da
Bence <hektor@Hektors-iMac.local>
2020-08-10 17:13:35 +0200

    a1.1.2

--- ize.java
+++ ize.java
@@ -12,9 +12,6 @@ public class ize {
         char[] strArr = str.toCharArray();
         StringBuilder returnStr = new StringBuilder();
         for (int i = 0; i<strArr.length; i++) {
-            // char temp = strArr[i];
-            // strArr[i] = strArr[(strArr.length-i)-1];
-            // strArr[(strArr.length-i)-1] = temp;
             returnStr.append(strArr[(strArr.length-i)-1]);
         }
 
