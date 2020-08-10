b20df814d30fc668a86ee1c8abd61d2795ae8cfd
Bence <hektor@Hektors-iMac.local>
2020-08-10 17:28:18 +0200

    a3

--- B.java
+++ B.java
@@ -1,8 +1,13 @@
 public class B {
+	public static void main(String[] args) {
+		
+	} 
+	
 	public void count(){
 		System.out.println("18");
 		String string = "test";
 		System.out.println("18"+ string);
+		System.out.println("September");
 		
 }
 }
