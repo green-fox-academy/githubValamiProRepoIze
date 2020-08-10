9e2076880b4cadfd4be26c95aab52a21ff3d86f2
Viktor Turkovics <arturo.el.guerrero.23@gmail.com>
2020-08-10 16:54:05 +0200

    d1'

---
+++ d.java
@@ -0,0 +1,9 @@
+public class d{
+    public static void main{
+        future();
+    }
+
+    public static void future(){
+      System.out.println("My horoscope/zodiac sign is pisces and the indian sign is wolf")
+    }
+}
